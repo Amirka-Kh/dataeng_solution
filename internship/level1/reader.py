8461a47cbadbc4e0a3728ff6a68610ab0cc4b2ec
Amir <66668728+Amirka-Kh@users.noreply.github.com>
2021-10-07 13:08:47 +0300

    processes new data to csv and db

--- internship/level1/reader.py
+++ internship/level1/reader.py
@@ -1,23 +1,43 @@
-import pandas as pd
-import requests
 import io
+import os.path
+import pandas as pd
+import psycopg2.extras
+import requests
+import numpy
+from psycopg2.extensions import register_adapter, AsIs
+
+
+def adapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+
+
+def adapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+
+
+register_adapter(numpy.float64, adapt_numpy_float64)
+register_adapter(numpy.int64, adapt_numpy_int64)
 
 url = "https://raw.githubusercontent.com/provectus/internship/main/dataeng/02-src-data/"
 order = ''
 index = 0
+conn = None  # to connect database
 
+# I keep dictionaries to check if data record has change
 try:
     temp = pd.read_csv('hash_id.csv')
     hash_id = temp.to_dict()
+    os.path.remove('./hash_id.csv')
     temp = pd.read_csv('img_paths.csv')
     img_urls = temp.to_dict()
+    os.path.remove('./img_paths.csv')
 except Exception as e:
-    print(e)
     hash_id = {}
     img_urls = {}
 
 try:
     output_file = pd.read_csv('output.csv')
+    os.path.remove('./output.csv')
 except Exception as e:
     output_file = pd.DataFrame()
 
@@ -45,7 +65,7 @@ while True:
             index += 1
             continue
         else:
-            output_file.at[index, 'img_path'] = img_path
+            output_file.at[int(index), 'img_path'] = img_path
             img_urls[index] = [img_path]
     else:
         hash_id[index] = [order]
@@ -60,3 +80,41 @@ table_id.to_csv('hash_id.csv')
 
 img_paths = pd.DataFrame.from_dict(img_urls)
 img_paths.to_csv('img_paths.csv')
+
+try:
+    with psycopg2.connect(dbname='p-data',
+                          user='postgres',
+                          password='postgres',
+                          host='localhost',
+                          port=5432) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            # for now it is the easiest to process new data
+            # it is very bad in terms of computational cost
+            cur.execute('DROP TABLE IF EXISTS users')
+
+            create_script = ''' CREATE TABLE IF NOT EXISTS users (
+                                    id bigint PRIMARY KEY,
+                                    first_name varchar(40) NOT NULL,
+                                    last_name varchar(40) NOT NULL,
+                                    birth bigint,
+                                    img_path varchar(100))'''
+            cur.execute(create_script)
+
+            output_list = list(output_file.to_records(index=False))
+            insert_script = '''INSERT INTO users (id, first_name, last_name, birth, img_path)
+                                VALUES (%s, %s, %s, %s, %s)'''
+            for record in output_list:
+                cur.execute(insert_script, record)
+
+            # update_script = "UPDATE users WHERE id = 1 SET img_path = 'www.youtube.com'"
+            # cur.execute(update_script)
+
+            cur.execute('SELECT * FROM users')
+            for record in cur.fetchall():
+                print(record)
+            conn.commit()
+except Exception as e:
+    print(e)
+finally:
+    if conn is not None:
+        conn.close()
